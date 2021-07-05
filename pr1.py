50923561008b6b73c7170db2eec8e647f6c6a5c2
Vaishnavi-Kocherla <86966848+Vaishnavi-Kocherla@users.noreply.github.com>
2021-07-05 19:01:13 +0530

    Update pr1.py

--- pr1.py
+++ pr1.py
@@ -2,10 +2,11 @@ import hashlib
 
 string_data = input("Enter string data: ")
 result = hashlib.md5(string_data.encode())
+
 print("The hash is ")
+
 print("hash in byte foramt")
 print(result.digest())
+
 print("hash in hexadecimal format")
 print(result.hexdigest())
-
-print(hashlib.algorithms_available)
